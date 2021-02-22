14fcbf5fb92ed5edcd656a0a5d242cf6c56b3dfc
wei756 <35715109+wei756@users.noreply.github.com>
2021-02-23 04:17:44 +0900

    더미 모듈 작성

---
+++ src/modules/bestwakki/Bestwakki.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Route, Link } from 'react-router-dom';
+import styled from "styled-components";
+
+class Bestwakki extends Component {
+
+  render() {
+    return (
+      <div className="Bestwakki">
+        bestwakki 모듈데스
+      </div>
+    );
+  }
+}
+
+export default Bestwakki;
---
+++ src/modules/live/Live.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Route, Link } from 'react-router-dom';
+import styled from "styled-components";
+
+class Live extends Component {
+
+  render() {
+    return (
+      <div className="Live">
+        Live 모듈데스
+      </div>
+    );
+  }
+}
+
+export default Live;
