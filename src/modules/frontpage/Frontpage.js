c5378ff182eb9009f1f36b3e1321f7c714f42fc6
wei756 <35715109+wei756@users.noreply.github.com>
2021-02-27 21:12:00 +0900

    Frontpage 패널 헤더/content 구분

--- src/modules/frontpage/Frontpage.js
+++ src/modules/frontpage/Frontpage.js
@@ -57,6 +57,8 @@ class ArticleList extends Component {
           <Link to={data.link}>
             <span className="tit">{data.title}</span><span className="more">더 보기 <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" width="18px" height="18px"><g><rect fill="none" height="24" width="24" y="0"/></g><g><g><g><path d="M9.29,15.46l3.88-3.88L9.29,7.7c-0.39-0.39-0.39-1.02,0-1.41l0,0c0.39-0.39,1.02-0.39,1.41,0l4.59,4.59 c0.39,0.39,0.39,1.02,0,1.41l-4.59,4.59c-0.39,0.39-1.02,0.39-1.41,0l0,0C8.91,16.49,8.9,15.85,9.29,15.46z"/></g></g></g></svg></span>
           </Link>
+        </div>
+        <div className="compContainer">
           {data.component}
         </div>
       </li>
