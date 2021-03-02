eb9058e5eb5e812557d8579dbe48899f8ee9966d
wei756 <35715109+wei756@users.noreply.github.com>
2021-03-03 00:12:14 +0900

    Dropdown 외부 클릭/리사이즈/스크롤 시 닫힘 기능, 모바일 디자인 구현, hover transition 적용

--- src/common/Components/Dropdown.js
+++ src/common/Components/Dropdown.js
@@ -68,6 +68,22 @@ class Dropdown extends Component {
     });
   }
 
+  onWindowScroll(e) {
+    if (e.state.opened) {
+      e.closeDropdownList();
+    }
+  }
+  onWindowResize(e) {
+    if (e.state.opened) {
+      e.closeDropdownList();
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', () => this.onWindowResize(this));
+    window.addEventListener('scroll', () => this.onWindowScroll(this));
+  }
+
   render() {
     const { x1, y1, x2, y2, name, value, opened } = this.state;
     const { values } = this.props;
@@ -105,6 +121,7 @@ class Dropdown extends Component {
           <span className="currName">{name}</span>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/></svg>
         </div>
+        <div className="closeArea" onClick={e => this.closeDropdownList()}></div>
         <DropdownArea className="dropdownArea">
           {list}
         </DropdownArea>
