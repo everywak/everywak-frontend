437d1e48233fe46d88b9d1cc4bdbe482dd2878b1
wei756 <35715109+wei756@users.noreply.github.com>
2021-03-12 00:47:59 +0900

    Bestwakki 더보기 구현

--- src/modules/bestwakki/Bestwakki.js
+++ src/modules/bestwakki/Bestwakki.js
@@ -17,26 +17,37 @@ class Bestwakki extends Component {
 
   state = {
     list: [],
-    loaded: false
+    loaded: false,
+    page: 1
   };
 
   fetchArticlesInfo = async ({ reset }) => {
+    this.setState({
+      loaded: false,
+    });
     const { search } = this.props.location || {};
-    const post = await service.getBestArticles(func.getURLParams(search));
+    const params = func.getURLParams(search);
+    params.page = reset ? 1 : this.state.page + 1;
+    const post = await service.getBestArticles(params);
 
     if (post.status === 200) {
+      const list = reset ? 
+        post.data.message.result.popularArticleList : 
+        this.state.list.concat(post.data.message.result.popularArticleList);
+      
       this.setState({
-        list: post.data.message.result.popularArticleList,
-        loaded: true
+        list: list,
+        loaded: true,
+        page: params.page,
       });
     }
   };
   componentDidMount() {
-    this.fetchArticlesInfo({reset: false});
+    this.fetchArticlesInfo({reset: true});
   }
   componentDidUpdate(prevProps) {
     if (this.props.location !== prevProps.location) {
-      this.fetchArticlesInfo({reset: false});
+      this.fetchArticlesInfo({reset: true});
     }
   }
 
@@ -52,7 +63,13 @@ class Bestwakki extends Component {
           </div>
         </div>
         <ArticleList front={this.props.front} data={this.state.list} loaded={this.state.loaded} />
-        <div className="more"><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" width="18px" height="18px"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><g><path d="M12,8c1.1,0,2-0.9,2-2s-0.9-2-2-2s-2,0.9-2,2S10.9,8,12,8z M12,10c-1.1,0-2,0.9-2,2s0.9,2,2,2s2-0.9,2-2S13.1,10,12,10z M12,16c-1.1,0-2,0.9-2,2s0.9,2,2,2s2-0.9,2-2S13.1,16,12,16z"/></g></g></g></svg></div>
+        <div className="more">
+          <svg className="frontOnly" enable-background="new 0 0 24 24" viewBox="0 0 24 24" width="18px" height="18px"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><g><path d="M12,8c1.1,0,2-0.9,2-2s-0.9-2-2-2s-2,0.9-2,2S10.9,8,12,8z M12,10c-1.1,0-2,0.9-2,2s0.9,2,2,2s2-0.9,2-2S13.1,10,12,10z M12,16c-1.1,0-2,0.9-2,2s0.9,2,2,2s2-0.9,2-2S13.1,16,12,16z"/></g></g></g></svg>
+          <div className="moreLoad" onClick={e => this.fetchArticlesInfo({reset: false})}>
+            <span>더 보기</span>
+            <svg viewBox="0 0 24 24" width="24px" height="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/></svg>
+          </div>
+        </div>
       </div>
     );
   }
@@ -98,8 +115,8 @@ class ArticleList extends Component {
       <ul className="ArticleList">
         <Article header={true} key={listHeader.articleId} data={listHeader} />
 
-        {this.props.loaded ? null : <Spinner caption="인기글 로딩중..."/>}
         {list}
+        {this.props.loaded ? null : <Spinner caption="인기글 로딩중..."/>}
       </ul>
     );
   }
@@ -273,10 +290,13 @@ class SearchBar extends Component {
 
   search() {
     if (this.props.history) {
+      const queryTxt = document.querySelector('input[name=queryTxt]');
+      var val = queryTxt.value ? queryTxt.value : queryTxt.dataset.defaultvalue;
+
       addURLParams.bind(this) (
         '/bestwakki', {
         queryTarget: document.querySelector('input[name=searchTarget]').value,
-        queryTxt: document.querySelector('input[name=queryTxt]').value,
+        queryTxt: val,
       });
     }
   }
@@ -302,10 +322,11 @@ class SearchBar extends Component {
     const phTxtList = [
       'manhwa', 'manhwa', 'ㅗㅜㅑ', '왁굳', 'ㅇㄷ', 'ㅇㅎ', '꿀팁', '꿀팁', 
     ];
+    const phTxt = phTxtList[Math.floor(Math.random() * phTxtList.length)];
 
     return (
       <div className="SearchBar" >
-        <input type="text" name="queryTxt" id="" className="inputForm" onKeyUp={e => {if (e.keyCode === 13) {this.search()}}} placeholder={"'" + phTxtList[Math.floor(Math.random() * phTxtList.length)] + "' 검색해 보기"} />
+        <input type="text" name="queryTxt" id="" className="inputForm" onKeyUp={e => {if (e.keyCode === 13) {this.search()}}} placeholder={"'" + phTxt + "' 검색해 보기"} data-defaultvalue={phTxt} />
         <Dropdown className="searchTarget" name="searchTarget" values={searchTarget} defaultName="제목" defaultValue="title" />
         <div className="btnSearch" onClick={e => this.search()}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white"><path d="M0 0h24v24H0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
