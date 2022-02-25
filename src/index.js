e3efa1188d0ffa679de731430b9bdaaac8527ae5
wei756 <kjhoon1122@naver.com>
2022-02-25 14:03:18 +0000

    IE 지원 제거
    - production 빌드시 번들 사이즈 3분의 1이나 감소됨 IEㅗ^^ㅗ

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
