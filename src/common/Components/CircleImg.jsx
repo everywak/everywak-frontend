60777c825af0f1d977cd0957b87bfb89d820c5c3
wei756 <kjhoon1122@naver.com>
2022-02-26 12:06:17 +0000

    CircleImg 인자 오류 수정
    - styled Image 컴포넌트 외부로 분리

--- src/common/Components/CircleImg.jsx
+++ src/common/Components/CircleImg.jsx
@@ -4,21 +4,26 @@ import styles from './CircleImg.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
+
+const Image = styled.img`
+object-fit: ${props => (['fill', 'contain', 'cover', 'none', 'scale-down'].includes(props.objectFit) ? props.objectFit : 'cover')};
+padding: ${props => props.padding}px;
+`;
+
 /**
  * 원 모양 이미지
  * 
- * @param {{src: string, alt?: string, objectFit: 'cover'|'contain', padding: number, ...props}} props 
+ * @param {{src: string, alt?: string, objectFit: 'cover'|'contain', padding: number}} props 
  */
-function CircleImg({className, src, alt, objectFit = 'cover', padding = 0, ...props}) {
+function CircleImg(props) {
+  const {
+    className, src, alt, objectFit = 'cover', padding = 0
+  } = props;
   
-  const Image = styled.img`
-    object-fit: ${['fill', 'contain', 'cover', 'none', 'scale-down'].includes(objectFit) ? objectFit : 'cover'};
-    padding: ${padding}px;
-  `;
 
   return (
-    <div className={cx('CircleImg', ...className)} {...props}>
-        <Image src={src} alt={alt} onError={e => {e.target.src = '/images/blank.png'}} />
+    <div className={cx('CircleImg', className)} {...props}>
+        <Image src={src} alt={alt} objectFit={objectFit} padding={padding} onError={e => {e.target.src = '/images/blank.png'}} />
     </div>
   );
 
