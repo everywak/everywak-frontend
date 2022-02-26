4e3a8581343967ce4fea216bb4e0fb3b30eecd41
wei756 <kjhoon1122@naver.com>
2022-02-26 06:58:24 +0000

    CircleImg 함수형 컴포넌트로 변환
    - props로부터 className 넘겨받을 수 있도록 수정

--- src/common/Components/CircleImg.jsx
+++ src/common/Components/CircleImg.jsx
@@ -1,32 +1,27 @@
-import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
 
 import styles from './CircleImg.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
-class CircleImg extends Component {
-  static defaultProps = {
-    src: '',
-    alt: '',
-    objectFit: 'cover', // cover | contain
-    padding: 0,
-  }
+/**
+ * 원 모양 이미지
+ * 
+ * @param {{src: string, alt?: string, objectFit: 'cover'|'contain', padding: number, ...props}} props 
+ */
+function CircleImg({className, src, alt, objectFit = 'cover', padding = 0, ...props}) {
   
-  render() {
-    const { src, alt, objectFit, padding } = this.props;
-    
-    const Image = styled.img`
-      object-fit: ${['fill', 'contain', 'cover', 'none', 'scale-down'].includes(objectFit) ? objectFit : 'cover'};
-      padding: ${padding}px;
-    `;
+  const Image = styled.img`
+    object-fit: ${['fill', 'contain', 'cover', 'none', 'scale-down'].includes(objectFit) ? objectFit : 'cover'};
+    padding: ${padding}px;
+  `;
 
-    return (
-      <div className="CircleImg" {...this.props}>
-          <Image src={src} alt={alt} onError={e => {e.target.src = '/images/blank.png'}} />
-      </div>
-    );
-  }
+  return (
+    <div className={cx('CircleImg', ...className)} {...props}>
+        <Image src={src} alt={alt} onError={e => {e.target.src = '/images/blank.png'}} />
+    </div>
+  );
+
 }
 
 export default CircleImg;
