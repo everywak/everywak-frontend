1357ac3118cec1c289317e2fb418b15ac8a02b5d
wei756 <kjhoon1122@naver.com>
2022-02-26 06:59:44 +0000

    Spinner 함수형 컴포넌트로 변환

--- src/common/Components/Spinner.js
+++ src/common/Components/Spinner.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
 import styles from './Spinner.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
-class Spinner extends Component {
-  static defaultProps = {
-    caption: '',
-    struct: null,
-    structLength: 3,
-  }
-  
+/**
+ * 
+ * @param {{caption: String, struct: JSX.Element, structLength: Number}} props 
+ */
+function Spinner({caption = '', struct, structLength = 3}) {
 
-  render() {
-    const { caption, struct, structLength } = this.props;
-    const spin = 
-    <div className="spinnerWrapper">
-      <img src="images/spinner.svg" className="spinnerAnim" alt="" />
-      <div className="caption">{caption}</div>
-    </div>;
-    const structs = [...Array(structLength).keys()].map(i => (struct));
+  const spin = 
+  <div className="spinnerWrapper">
+    <img src="images/spinner.svg" className="spinnerAnim" alt="" />
+    <div className="caption">{caption}</div>
+  </div>;
+  const structs = [...Array(structLength).keys()].map(i => (struct));
 
-    return (
-      <div className="Spinner">
-        {struct ? structs : spin}
-      </div>
-    );
-  }
+  return (
+    <div className="Spinner">
+      {struct ? structs : spin}
+    </div>
+  );
+
 }
 
 export default Spinner;
