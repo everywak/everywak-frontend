2edac3539b0a9544e385034ea894ac681aabe0b0
wei756 <kjhoon1122@naver.com>
2022-02-27 07:21:02 +0000

    Frontpage 함수형 컴포넌트화

--- src/modules/frontpage/Frontpage.js
+++ src/modules/frontpage/Frontpage.js
@@ -1,73 +1,62 @@
-import React, { Component } from 'react';
-
 import FrontPanel from './FrontPanel';
 import Bestwakki from '../bestwakki/Bestwakki.js';
 import Live from '../live/Live.js';
 import Apps from '../apps/Apps.js';
 import Etcs from '../apps/Etcs.js';
-import { Link } from 'react-router-dom';
 
 import styles from './Frontpage.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
-class Frontpage extends Component {
-
-  render() {
-    document.title = '에브리왁굳 : 메인';
-    
-    return (
-      <div className="Frontpage">
-        <FrontPanelList />
-      </div>
-    );
-  }
+function Frontpage() {
+  document.title = '에브리왁굳 : 메인';
+  
+  return (
+    <div className="Frontpage">
+      <FrontPanelList />
+    </div>
+  );
 }
 
-class FrontPanelList extends Component {
-  static defaultProps = {
-    data: [
-      {
-        id: 'front_live',
-        title: "우왁굳 생방송",
-        moreable: true,
-        link: "/live",
-        component: <Live front={true} />
-      },
-      {
-        id: 'front_bestwakki',
-        title: "왁물원 인기글",
-        moreable: true,
-        link: "/bestwakki",
-        component: <Bestwakki front={true} />
-      },
-      {
-        id: 'front_apps',
-        title: "여러가지가 있읍니다",
-        moreable: false,
-        component: <Apps front={true} />
-      },
-      {
-        id: 'front_etcs',
-        title: "딱히.. 팬치쿤이 봐줬으면 하는 건 아니고.... ",
-        moreable: false,
-        component: <Etcs front={true} />
-      },
-    ]
-  }
+function FrontPanelList() {
+  const data = [
+    {
+      id: 'front_live',
+      title: "우왁굳 생방송",
+      moreable: true,
+      link: "/live",
+      component: <Live front={true} />
+    },
+    {
+      id: 'front_bestwakki',
+      title: "왁물원 인기글",
+      moreable: true,
+      link: "/bestwakki",
+      component: <Bestwakki front={true} />
+    },
+    {
+      id: 'front_apps',
+      title: "여러가지가 있읍니다",
+      moreable: false,
+      component: <Apps front={true} />
+    },
+    {
+      id: 'front_etcs',
+      title: "딱히.. 팬치쿤이 봐줬으면 하는 건 아니고.... ",
+      moreable: false,
+      component: <Etcs front={true} />
+    },
+  ];
 
-  render() {
-    const { data } = this.props;
-    const list = data.map(
-      data => <FrontPanel key={data.id} data={data} />
-    );
+  const list = data.map(
+    data => <FrontPanel key={data.id} data={data} />
+  );
 
-    return (
-      <ul className="FrontPanelList">
-        {list}
-      </ul>
-    );
-  }
+  return (
+    <ul className="FrontPanelList">
+      {list}
+    </ul>
+  );
 }
 
 export default Frontpage;
