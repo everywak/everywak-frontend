c487fa755024b345687754be492c308d44e913a4
wei756 <kjhoon1122@naver.com>
2022-03-01 06:14:23 +0000

    이세돌 클립 최근 7일것만 표시되게 수정

--- src/services/Isedol.js
+++ src/services/Isedol.js
@@ -24,7 +24,15 @@ export async function getYoutubeVideos() {
 
 export async function getTwitchClips() {
   try {
-    const res = await api('/isedol/TwitchClips?orderBy=view');
+    const options = {
+      mode: 'cors',
+      params: {beginAt: Date.now() / 1000 - 7*24*60*60, endAt: Date.now() / 1000}, 
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8'
+      }
+    };
+    const res = await api('/isedol/TwitchClips?orderBy=view', options);
     const { message } = res.data;
 
     if (message && message.status == 200) {
