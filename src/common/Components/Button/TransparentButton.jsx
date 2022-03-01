fb381eb488a39201c56eeced0bb8dcd11e9ab391
wei756 <kjhoon1122@naver.com>
2022-03-01 04:46:28 +0000

    [#12] TransparentButton 구현

---
+++ src/common/Components/Button/TransparentButton.jsx
@@ -0,0 +1,28 @@
+
+import BasicButton from './BasicButton';
+
+import classNames from 'classnames/bind';
+const cx = classNames;
+
+/**
+ * 투명 버튼
+ * 
+ * @param {{className?: string, onClick?: function, children?: JSX.Element|String}} props 
+ */
+function TransparentButton(props) {
+  
+  const {
+    background, 
+    className, 
+    children, 
+    ...rest
+  } = props;
+
+  return (
+    <BasicButton className={cx("Transparent", className)} {...rest}>
+      {children}
+    </BasicButton>
+  );
+}
+
+export default TransparentButton;
