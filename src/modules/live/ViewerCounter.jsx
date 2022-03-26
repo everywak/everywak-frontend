d938ff83d0a4dd0ccf5d128f890389ab84ca35e5
wei756 <kjhoon1122@naver.com>
2022-03-26 07:44:57 +0000

    Live 최고 시청자수 표시 기능 구현

--- src/modules/live/ViewerCounter.jsx
+++ src/modules/live/ViewerCounter.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RemoveRedEyeRoundedIcon from '@material-ui/icons/RemoveRedEyeRounded';
 
 import styles from './ViewerCounter.scss';
 
 export default function ViewerCounter({viewer = 0}) {
+
+  const [maxViewer, setMaxViewer] = useState(0);
+
+  useEffect(() => {
+    if (viewer > maxViewer) {
+      setMaxViewer(viewer);
+    }
+  }, [viewer]);
   const formattedViewer = viewer.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+  const formattedMaxViewer = maxViewer.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
   return (<div className="ViewerCounter">
     <RemoveRedEyeRoundedIcon fontSize="small" />
     <span className="counterWrapper">
       {formattedViewer}
     </span>
+    <div className="maxViewerWrapper">
+      최고 시청자수: {formattedMaxViewer}명
+    </div>
   </div>);
 }
