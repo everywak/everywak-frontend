b402b80ba326e31d450884ad8313f5ce3717f557
wei756 <kjhoon1122@naver.com>
2022-03-27 14:09:58 +0000

    LiveApi 주석 추가
    - return 타입 정의

--- src/services/LiveWakApi.js
+++ src/services/LiveWakApi.js
@@ -5,6 +5,18 @@ import * as func from '../common/funtions';
 const api = axios.create({
   baseURL: 'https://api.everywak.kr',
 })
+
+/**
+ * @typedef WakLiveInfoItem
+ * @property {'TWITCH'|'YOUTUBE'|'NONE'} broadcaster
+ * @property {string} startedTime
+ * @property {string} thumbnail
+ * @property {string} title
+ * @property {number} viewerCount
+ */
+/**
+ * @returns {WakLiveInfoItem[]}
+ */
 export async function getBroadcastInfo() {
   try {
     const res = await api(`/live/LiveWakInfo?salt=${parseInt(Date.now() / 1000 / 30)}`);
@@ -24,7 +36,19 @@ export async function getBroadcastInfo() {
     };
   }
 }
-
+/**
+ * @typedef BroadcastInfoItem
+ * @property {'TWITCH'|'YOUTUBE'} broadcaster
+ * @property {string} login_name
+ * @property {string} nickname
+ * @property {string} startedTime
+ * @property {string} thumbnail
+ * @property {string} title
+ * @property {number} viewerCount
+ */
+/**
+ * @returns {BroadcastInfoItem[]}
+ */
 export async function getWaktaverseBroadcastInfo() {
   try {
     const res = await api('/live/WaktaverseLiveInfo');
@@ -40,6 +64,22 @@ export async function getWaktaverseBroadcastInfo() {
     return [];
   }
 }
+/**
+ * @typedef MemberInfoItem
+ * @property {string} broadcaster_type
+ * @property {string} created_at
+ * @property {string} description
+ * @property {string} display_name
+ * @property {string} id
+ * @property {string} login
+ * @property {string} offline_image_url
+ * @property {string} profile_image_url
+ * @property {string} type
+ * @property {number} viewerCount
+ */
+/**
+ * @returns {MemberInfoItem[]}
+ */
 export async function getWaktaverseInfo() {
   try {
     const res = await api('/live/WaktaverseInfo');
