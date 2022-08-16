d2e83300118e6871840bb70e38274f7e515f4123
wei756 <kjhoon1122@naver.com>
2022-08-16 20:56:35 +0900

    HorizontalScrollableList 구현
    - PC환경에서 가로 스크롤할 수 있게 조작버튼이 내장된 가로 리스트

---
+++ src/common/Components/HorizontalScrollableList/HorizontalScrollableList.jsx
@@ -0,0 +1,89 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+
+import TransparentButton from '../Button/TransparentButton';
+import KeyboardArrowLeftRoundedIcon from '@material-ui/icons/KeyboardArrowLeftRounded';
+import KeyboardArrowRightRoundedIcon from '@material-ui/icons/KeyboardArrowRightRounded';
+
+import * as func from '../../funtions';
+
+import styles from './HorizontalScrollableList.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+
+const SCROLL_DISABLE = -1;
+const SCROLL_START = 0;
+const SCROLL_MIDDLE = 1;
+const SCROLL_END = 2;
+
+/**
+ * PC 환경을 지원하는 가로 스크롤링 리스트
+ * 
+ * @param {{
+ * children?: JSX.Element|String,
+ * backgroundColor: String,
+ * scrollAmount: Number,
+ * controlWidth: Number,
+ * }} props 
+ */
+function HorizontalScrollableList({ children, backgroundColor = '#ffffff', scrollAmount = 300, controlWidth = 64, scrollThreshold = 3 }) {
+
+  const [scrollState, setScrollState] = useState(SCROLL_DISABLE);
+
+  const refContent = useRef();
+
+  useEffect(() => {
+    const loop = setInterval(loopScrollHandler, 100);
+    return () => {
+      clearInterval(loop);
+    }
+  }, [loopScrollHandler]);
+
+  const loopScrollHandler = useCallback(() => {
+
+    if (!refContent.current) { return; }
+
+    const { 
+      clientWidth, scrollLeft, scrollWidth, 
+    } = refContent.current;
+    if (clientWidth === scrollWidth) {
+      setScrollState(SCROLL_DISABLE);
+    } else if (scrollLeft < scrollThreshold) {
+      setScrollState(SCROLL_START);
+    } else if (scrollLeft + clientWidth > scrollWidth - scrollThreshold) {
+      setScrollState(SCROLL_END);
+    } else {
+      setScrollState(SCROLL_MIDDLE);
+    }
+  }, [scrollState]);
+  
+  function onClickScroll(rot) {
+    if (refContent.current) {
+      refContent.current.scrollTo({
+        left: refContent.current.scrollLeft + rot * scrollAmount,
+        behavior: 'smooth'
+      })
+    }
+  }
+
+  const listStyle = {
+    '--backgroundColor': backgroundColor,
+    '--backgroundTransparentColor': backgroundColor + '00',
+    '--controlWidth': controlWidth + 'px',
+  };
+
+  return (
+    <div className={cx('HorizontalScrollableList')} style={listStyle}>
+      <div className="listWrapper" ref={refContent}>
+        {children}
+      </div>
+        <TransparentButton className={cx('btnControl', 'btnScrollLeft', {hideBtn: !(scrollState == SCROLL_MIDDLE || scrollState == SCROLL_END)})} onClick={e => onClickScroll(-1)}>
+          <KeyboardArrowLeftRoundedIcon fontSize="large" />
+        </TransparentButton>
+        <TransparentButton className={cx('btnControl', 'btnScrollRight', {hideBtn: !(scrollState == SCROLL_MIDDLE || scrollState == SCROLL_START)})} onClick={e => onClickScroll(1)}>
+          <KeyboardArrowRightRoundedIcon fontSize="large" />
+        </TransparentButton>
+    </div>
+  );
+}
+
+export default HorizontalScrollableList;
