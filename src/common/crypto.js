a37328130cf4765fbef9597ef93ded783920575d
wei756 <kjhoon1122@naver.com>
2022-08-20 14:36:07 +0900

    aec 암호화/복호화 함수 구현

---
+++ src/common/crypto.js
@@ -0,0 +1,21 @@
+import CryptoJS from 'crypto-js';
+
+/**
+ * AES 알고리즘으로 암호화합니다.
+ * 
+ * @param {String} data 
+ * @param {String} key 
+ * @returns {String}
+ */
+const aesEncrypt = (data, key) => CryptoJS.AES.encrypt(String(data), key).toString();
+
+/**
+ * AES 알고리즘으로 복호화합니다.
+ * 
+ * @param {String} data 
+ * @param {String} key 
+ * @returns {String}
+ */
+const aesDecrypt = (data, key) => CryptoJS.AES.decrypt(String(data), key).toString(CryptoJS.enc.Utf8);
+
+export { aesEncrypt, aesDecrypt };
