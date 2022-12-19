abbef1cb4ad93d23263a9463abef4f902c5b2e91
wei756 <kjhoon1122@naver.com>
2022-12-19 23:53:51 +0900

    Weather 이전 날짜 뱅온정보 나이순 정렬 적용

--- src/modules/weather/PrevWeatherList.jsx
+++ src/modules/weather/PrevWeatherList.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
+import { orderNickname } from './weather.common';
+
 import PrevWeatherItem from './PrevWeatherItem';
 
 import SkeletonLoader from '../../common/Components/SkeletonLoader';
@@ -19,7 +21,10 @@ const skeleton = <PrevWeatherItem weather="sunny" isSkeleton />;
  */
 function PrevWeatherList({ date = '000000', items, isLoading }) {
 
-  const list = items && items.map(item => <PrevWeatherItem key={item.nickname} name={item.nickname} state={item.rawInfo} weather={item.weather} />);
+  const list = items && 
+    items
+    .sort((a, b) => orderNickname[a.nickname] - orderNickname[b.nickname])
+    .map(item => <PrevWeatherItem key={item.nickname} name={item.nickname} state={item.rawInfo} weather={item.weather} />);
 
   const dateTarget = new Date('20' + date.slice(0, 2), parseInt(date.slice(2, 4)) - 1, date.slice(4, 6));
   const dateString = `${dateTarget.getMonth() + 1}/${dateTarget.getDate()}(${'일월화수목금토'[dateTarget.getDay()]})`;
