2ccb535b7410e76d288d5cb98dbbca5495c8145b
wei756 <kjhoon1122@naver.com>
2023-02-04 19:10:10 +0900

    BasicButton 리본메세지 기능 구현

--- src/common/Components/Button/BasicButton.jsx
+++ src/common/Components/Button/BasicButton.jsx
@@ -7,21 +7,25 @@ const cx = classNames.bind(styles);
 /**
  * 기본 버튼
  * 
- * @param {{background?: string, className?: string, onClick?: function, children?: JSX.Element|String}} props 
+ * @param {{background?: string, className?: string, description?: string, onClick?: function, children?: JSX.Element|String}} props 
  */
 function BasicButton(props) {
   
   const {
     background,
     className, 
+    description, 
     onClick, 
     children, 
     ...rest
   } = props;
 
   return (
-    <button className={cx("BasicButton", className)} onClick={onClick} {...rest} style={{background: background}}>
+    <button className={cx("BasicButton", className, {hasRibbon: description})} onClick={onClick} {...rest} style={{background: background}}>
       {children}
+      {description &&
+        <div className="description">{description}</div>
+      }
     </button>
   );
 }
