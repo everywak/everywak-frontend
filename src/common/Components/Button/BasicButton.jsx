e96f35c4e4fd0d4e96595eadd254ca4288e5e290
wei756 <kjhoon1122@naver.com>
2023-07-14 05:04:01 +0900

    Button disabled props 추가

--- src/common/Components/Button/BasicButton.jsx
+++ src/common/Components/Button/BasicButton.jsx
@@ -7,7 +7,7 @@ const cx = classNames.bind(styles);
 /**
  * 기본 버튼
  * 
- * @param {{background?: string, className?: string, description?: string, onClick?: function, children?: JSX.Element|String}} props 
+ * @param {{background?: string, className?: string, description?: string, onClick?: function, disabled: boolean, children?: JSX.Element|String}} props 
  */
 function BasicButton(props) {
   
@@ -17,11 +17,12 @@ function BasicButton(props) {
     description, 
     onClick, 
     children, 
+    disabled,
     ...rest
   } = props;
 
   return (
-    <button className={cx("BasicButton", className, {hasRibbon: description})} onClick={onClick} {...rest} style={{background: background}}>
+    <button className={cx("BasicButton", className, {hasRibbon: description, disabled})} onClick={onClick} {...rest} style={{background: background}} disabled={disabled}>
       {children}
       {description &&
         <div className="description">{description}</div>
