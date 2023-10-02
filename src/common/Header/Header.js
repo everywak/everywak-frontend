faba8b8736ebbb57f6c0392ec745138a89a00262
wei756 <kjhoon1122@naver.com>
2023-10-03 00:53:24 +0900

    로고 컴포넌트화

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -10,13 +10,17 @@ class Header extends Component {
     return (
       <header className="header">
         <div className="left"></div>
-        <Link to="/" className="title"><img src="/images/everywak_logo.png" alt="Everywak 로고"/></Link>
+        <EverywakLogo />
         <div className="right">
           <button className="moreApp"><img src="/images/grid_view-black-18dp.svg" alt="앱 바로가기 메뉴"/></button>
         </div>
       </header>
     );
   }
+}
+
+export function EverywakLogo (props) {
+  return <Link to="/" className="title"><img src="/images/everywak_logo.png" alt="Everywak 로고"/></Link>;
 }
 
 export default Header;
