31f22c75506b011334da3e38ea19d7162112a03f
wei756 <kjhoon1122@naver.com>
2023-10-06 17:02:14 +0900

    AppListItem 함수형 컴포넌트로 변환

--- src/modules/apps/AppListItem.jsx
+++ src/modules/apps/AppListItem.jsx
@@ -7,35 +7,30 @@ import styles from './AppListItem.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
-class AppListItem extends Component {
-  static defaultProps = {
-    title: '제목',
-    description: '어쩌구저쩌구 설명',
-    icon: null,
-    iconPadding: '0px',
-    themeColor: '#d3d3d3',
-    href: '',
-  }
+function AppListItem ({
+  title = '제목',
+  description = '어쩌구저쩌구 설명',
+  icon = null,
+  iconPadding = '0px',
+  themeColor = '#d3d3d3',
+  href = '',
+}) {
 
-  render() {
-    const { title, description, icon, iconPadding, themeColor, href } = this.props;
+  const content = <>
+    <div className="thumbWrapper">
+      <AppListItemIcon src={icon} alt={title} iconPadding={iconPadding} themeColor={themeColor} />
+    </div>
+    <div className="descWrapper">
+      <div className="title">{title}</div>
+      <div className="description">{description}</div>
+    </div>
+  </>;
 
-    const content = <React.Fragment>
-      <div className="thumbWrapper">
-        <AppListItemIcon src={icon} alt={title} iconPadding={iconPadding} themeColor={themeColor} />
-      </div>
-      <div className="descWrapper">
-        <div className="title">{title}</div>
-        <div className="description">{description}</div>
-      </div>
-    </React.Fragment>;
-
-    return (
-      href.slice(0, 4) == 'http' ? 
-      <a href={href} className="AppListItem">{content}</a> :
-      <Link to={href} className="AppListItem">{content}</Link> 
-    );
-  }
+  return (
+    href.slice(0, 4) == 'http' ? 
+    <a href={href} className={cx('AppListItem')}>{content}</a> :
+    <Link to={href} className={cx('AppListItem')}>{content}</Link> 
+  );
 }
   
 export default AppListItem;
