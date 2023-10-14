ff8a393b49eac30d6bb85cd6c64c5e361cee0a5f
wei756 <kjhoon1122@naver.com>
2023-10-15 05:48:10 +0900

    LivePreview 함수형 컴포넌트로 변환
    - useQuery 적용
    - BasicImage 적용
    - module scss 적용

--- src/modules/isedol/LivePreview.jsx
+++ src/modules/isedol/LivePreview.jsx
@@ -1,86 +1,92 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
 import Spinner from '../../common/Components/Spinner';
 import CircleImg from '../../common/Components/CircleImg';
+import BasicImage from '../../common/Components/Image/BasicImage';
 
 import * as service from '../../services/LiveWakApi';
 import * as everywakApi from '../../services/everywak-api/index';
 
-import styles from './LivePreview.scss';
+import styles from './LivePreview.module.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
-class LivePreview extends Component {
-  static defaultProps = {
-    channelId: 'twitchdev',
-    size: 'normal',
-  };
+function LivePreview({ channelId = 'twitchdev', size = 'normal' }) {
+  const fetchWaktaverseInfo = async () => {
+    const resWaktaverseInfo = await everywakApi.live.getWaktaverseInfo({
+      loginName: channelId
+    });
 
-  state = {
-    loaded: false, 
-    live: false,
-    previewImgUrl: '',
-    profileImgUrl: '',
-  };
-
-  constructor (props) {
-    super(props);
-  };
-
-  updateChannelImgs = async loginName => {
-
-    const waktaverseInfo = (await everywakApi.live.getWaktaverseInfo()).message.result;
-    const broadcast = await service.getWaktaverseBroadcastInfo();
-    const targetBroadcast = broadcast.find(item => item.loginName === loginName);
-    const targetInfo = waktaverseInfo?.find(item => item.twitchLoginName === loginName);
-    const thWidth = this.props.size === 'big' ? 600 : 200;
-    
-    if (targetBroadcast && targetInfo) { // 뱅온
-      const thumbnail = targetBroadcast.thumbnail.replace('{width}', thWidth).replace('{height}', parseInt(thWidth / 16 * 9));
-      this.setState({
-        loaded: true,
-        live: true,
-        previewImgUrl: thumbnail,
-        profileImgUrl: targetInfo.twitchProfileImage,
-      });
-    } else { // 뱅없
-      this.setState({
-        loaded: true,
-        live: false,
-        previewImgUrl: targetInfo.twitchOfflineImage.replace('1920x1080', `${thWidth}x${parseInt(thWidth / 16 * 9)}`),
-        profileImgUrl: targetInfo.twitchProfileImage,
-      });
+    if (
+      resWaktaverseInfo.message.status !== 200 ||
+      !resWaktaverseInfo.message.result[0]
+    ) {
+      throw resWaktaverseInfo;
     }
-  }
 
-  componentDidMount() {
-    this.updateChannelImgs(this.props.channelId);
-  }
+    const broadcast = await service.getWaktaverseBroadcastInfo();
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return true;
-  }
-
-  componentWillUnmount() {
-  }
-
-  render() {
-    const { channelId } = this.props;
-    const { loaded, live, previewImgUrl, profileImgUrl } = this.state;
-    
-    return (
-      <li className={cx('LivePreview', {loading: !loaded}, {off: !live})}>
-        <Link to={channelId === 'woowakgood' ? '/live' : `/withlive/isedol?main=${channelId}`}>
-          <img className="previewImg" src={previewImgUrl} alt="생방송 썸네일" onError={e => {e.target.src = '/images/blank.png'}} />
-          <div className="profileCircle">
-            <CircleImg className="innerCircle" src={profileImgUrl} alt="채널 프로필 이미지" />
-          </div>
-          <Spinner />
-        </Link>
-      </li>
+    const targetInfo = resWaktaverseInfo.message.result[0];
+    const targetBroadcast = broadcast.find(
+      item => item.loginName === channelId
     );
-  }
+    const thWidth = size === 'big' ? 600 : 200;
+
+    if (targetBroadcast && targetInfo) {
+      // 뱅온
+      const thumbnail = targetBroadcast.thumbnail
+        .replace('{width}', thWidth)
+        .replace('{height}', parseInt((thWidth / 16) * 9));
+      return {
+        isLive: true,
+        previewImgUrl: thumbnail,
+        profileImgUrl: targetInfo.twitchProfileImage
+      };
+    } else {
+      // 뱅없
+      return {
+        isLive: false,
+        previewImgUrl: targetInfo.twitchOfflineImage.replace(
+          '1920x1080',
+          `${thWidth}x${parseInt((thWidth / 16) * 9)}`
+        ),
+        profileImgUrl: targetInfo.twitchProfileImage
+      };
+    }
+  };
+
+  /**
+   * @type {{ data: {isLive: boolean, previewImgUrl: string, profileImgUrl: string }, isLoading: boolean }}
+   */
+  const { isLoading, data } = useQuery(
+    [`waktaverseInfo-${channelId}`],
+    fetchWaktaverseInfo
+  );
+
+  const url =
+    channelId === 'woowakgood' ? '/live' : `/withlive/isedol?main=${channelId}`;
+
+  return (
+    <li className={cx('LivePreview', { loading: isLoading }, { off: !data?.isLive })}>
+      <Link to={url}>
+        <BasicImage
+          className={styles.previewImg}
+          src={data?.previewImgUrl}
+          alt="생방송 썸네일"
+        />
+        <div className={styles.profileCircle}>
+          <CircleImg
+            className={styles.innerCircle}
+            src={data?.profileImgUrl}
+            alt="채널 프로필 이미지"
+          />
+        </div>
+        <Spinner className={styles.spinner} />
+      </Link>
+    </li>
+  );
 }
-  
-  export default LivePreview;
+
+export default LivePreview;
