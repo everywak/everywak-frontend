819c456ac8aa62fa63d34e490f08c57504864b43
wei756 <kjhoon1122@naver.com>
2023-10-16 01:28:15 +0900

    live getWaktaverseLiveInfo 구현

--- src/services/everywak-api/modules/live.js
+++ src/services/everywak-api/modules/live.js
@@ -3,6 +3,19 @@ import * as func from '../../../common/funtions';
 import { request } from '../common';
 
 /**
+ * @typedef DatabaseLiveItem
+ * @property {string} broadcasterId PK
+ * @property {number} updatedTimeStamp
+ * @property {'TWITCH'|'YOUTUBE'|'NONE'} broadcaster
+ * @property {string} title
+ * @property {number} viewerCount
+ * @property {number} startedTime
+ * @property {string} thumbnail
+ * @property {string} videoId
+ * @property {string} nickname
+ * @property {string} loginName
+ */
+/**
  * @typedef DatabaseMemberItem
  * @property {number} updatedTimeStamp
  * @property {string} broadcasterId PK
@@ -59,4 +72,38 @@ const getWaktaverseInfo = async params =>
     params: filterWaktaverseInfoParams(params)
   });
 
-export { getWaktaverseInfo };
+/**
+ * 왁타버스 생방송 정보 api 요청시 입력되는 파라미터 값을 필터링합니다.
+ *
+ * @see getWaktaverseLiveInfo
+ * @param {{[key: string]: string}} query
+ */
+const filterWaktaverseLiveInfoParams = query => {
+
+  const filteredParams = {
+  };
+
+  if (query?.loginName) {
+    filteredParams.loginName = query.loginName.replace(/[^0-9a-zA-Z_]/g, '');
+  }
+  if (query?.broadcasterId) {
+    filteredParams.broadcasterId = query.broadcasterId.replace(/[^0-9]/g, '');
+  }
+
+  return filteredParams;
+};
+
+/**
+ * 왁타버스 생방송 정보를 불러옵니다.
+ *
+ * @param {*} params
+ * @returns {Promise<import('../common').CommonMessage<DatabaseLiveItem[]>}
+ */
+const getWaktaverseLiveInfo = async params =>
+  await request({
+    method: 'GET',
+    uri: '/live/WaktaverseLiveInfo',
+    params: filterWaktaverseLiveInfoParams(params)
+  });
+
+export { getWaktaverseInfo, getWaktaverseLiveInfo };
