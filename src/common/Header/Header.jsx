3afadd674158149b1e22caeca347a50e24d867b6
wei756 <kjhoon1122@naver.com>
2024-05-12 09:07:23 +0900

    header module scss 적용 제거

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import styles from './Header.module.scss';
-import classNames from 'classnames/bind';
-const cx = classNames.bind(styles);
+import './Header.scss';
 
 function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.left}></div>
+    <header className="header">
+      <div className="left"></div>
       <EverywakLogo />
-      <div className={styles.right}>
-        {/* <button className={styles.moreApp}><img src="/images/grid_view-black-18dp.svg" alt="앱 바로가기 메뉴"/></button> */}
+      <div className="right">
+        {/* <button className="moreApp"><img src="/images/grid_view-black-18dp.svg" alt="앱 바로가기 메뉴"/></button> */}
       </div>
     </header>
   );
 }
 
 export function EverywakLogo (props) {
-  return <Link to="/" className={styles.title}><img src="/images/everywak_logo.png" alt="Everywak 로고"/></Link>;
+  return <Link to="/" className="title"><img src="/images/everywak_logo.png" alt="Everywak 로고"/></Link>;
 }
 
 export default Header;
