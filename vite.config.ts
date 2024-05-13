19ed22b1827e3b53c52d0b4f56bb947777f48674
wei756 <kjhoon1122@naver.com>
2024-05-13 11:03:05 +0900

    불필요 패키지 제거

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 import sass from 'sass';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import mkcert from 'vite-plugin-mkcert';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), mkcert(), nodePolyfills()],
+  plugins: [react(), tsconfigPaths(), mkcert()],
   css: {
     preprocessorOptions: {
       scss: {
