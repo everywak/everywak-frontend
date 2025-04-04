aef9fa2a6c736fc809cd523734c83ec8c902f9bd
wei756 <kjhoon1122@naver.com>
2025-04-04 21:46:57 +0900

    채팅 헤더 정리

--- src/components/withlive/LiveChat/components/Header.tsx
+++ src/components/withlive/LiveChat/components/Header.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-
-import classNames from 'classnames';
-import styles from './Header.module.scss';
 import { Button } from 'common/components';
 import { useLiveChatActions } from '../LiveChat.context';
 import { KeyboardTabRounded, SettingsRounded } from '@mui/icons-material';
-const cx = classNames.bind(styles);
+import styles from './Header.module.scss';
 
-export type Props = {};
-export function Header(props: Props) {
+export function Header() {
   const { setOpenedSetting } = useLiveChatActions();
+
   return (
-    <div className={styles.Header}>
+    <div className={styles.container}>
       <Button color="black-transparent">
         <KeyboardTabRounded />
       </Button>
