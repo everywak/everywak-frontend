3c60a355a1eedfaeed9e69d173a51ff7d31a91fa
wei756 <kjhoon1122@naver.com>
2025-04-06 05:42:07 +0900

    타입스크립트 변환

--- src/common/Components/HorizontalScrollableList/HorizontalScrollableList.tsx
+++ src/common/Components/HorizontalScrollableList/HorizontalScrollableList.tsx
@@ -1,10 +1,14 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, {
+  CSSProperties,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
 import TransparentButton from '../Button/TransparentButton';
 import KeyboardArrowLeftRoundedIcon from '@mui/icons-material/KeyboardArrowLeftRounded';
 import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
-
-import * as func from '../../functions';
 
 import './HorizontalScrollableList.scss';
 import cx from 'classnames';
@@ -14,29 +18,34 @@ const SCROLL_START = 0;
 const SCROLL_MIDDLE = 1;
 const SCROLL_END = 2;
 
+export interface Props {
+  className?: string;
+  children?: React.ReactNode;
+  backgroundColor?: string;
+  scrollAmount?: number;
+  controlWidth?: number;
+  scrollThreshold?: number;
+}
 /**
  * PC 환경을 지원하는 가로 스크롤링 리스트
- * 
- * @param {{
- * children?: JSX.Element|String,
- * backgroundColor: String,
- * scrollAmount: Number,
- * controlWidth: Number,
- * }} props 
  */
-function HorizontalScrollableList({ children, backgroundColor = '#ffffff', scrollAmount = 300, controlWidth = 64, scrollThreshold = 3 }) {
-
+function HorizontalScrollableList({
+  children,
+  backgroundColor = '#ffffff',
+  scrollAmount = 300,
+  controlWidth = 64,
+  scrollThreshold = 3,
+}: Props) {
   const [scrollState, setScrollState] = useState(SCROLL_DISABLE);
 
-  const refContent = useRef();
+  const refContent = useRef<HTMLDivElement>(null);
 
   const loopScrollHandler = useCallback(() => {
+    if (!refContent.current) {
+      return;
+    }
 
-    if (!refContent.current) { return; }
-
-    const { 
-      clientWidth, scrollLeft, scrollWidth, 
-    } = refContent.current;
+    const { clientWidth, scrollLeft, scrollWidth } = refContent.current;
     if (clientWidth === scrollWidth) {
       setScrollState(SCROLL_DISABLE);
     } else if (scrollLeft < scrollThreshold) {
@@ -47,20 +56,20 @@ function HorizontalScrollableList({ children, backgroundColor = '#ffffff', scrol
       setScrollState(SCROLL_MIDDLE);
     }
   }, [scrollState]);
-  
+
   useEffect(() => {
     const loop = setInterval(loopScrollHandler, 100);
     return () => {
       clearInterval(loop);
-    }
+    };
   }, [loopScrollHandler]);
-  
-  function onClickScroll(rot) {
+
+  function onClickScroll(rot: 1 | -1) {
     if (refContent.current) {
       refContent.current.scrollTo({
         left: refContent.current.scrollLeft + rot * scrollAmount,
-        behavior: 'smooth'
-      })
+        behavior: 'smooth',
+      });
     }
   }
 
@@ -68,19 +77,31 @@ function HorizontalScrollableList({ children, backgroundColor = '#ffffff', scrol
     '--backgroundColor': backgroundColor,
     '--backgroundTransparentColor': backgroundColor + '00',
     '--controlWidth': controlWidth + 'px',
-  };
+  } as CSSProperties;
 
   return (
     <div className={cx('HorizontalScrollableList')} style={listStyle}>
       <div className="listWrapper" ref={refContent}>
         {children}
       </div>
-        <TransparentButton className={cx('btnControl', 'btnScrollLeft', {hideBtn: !(scrollState == SCROLL_MIDDLE || scrollState == SCROLL_END)})} onClick={e => onClickScroll(-1)}>
-          <KeyboardArrowLeftRoundedIcon fontSize="large" />
-        </TransparentButton>
-        <TransparentButton className={cx('btnControl', 'btnScrollRight', {hideBtn: !(scrollState == SCROLL_MIDDLE || scrollState == SCROLL_START)})} onClick={e => onClickScroll(1)}>
-          <KeyboardArrowRightRoundedIcon fontSize="large" />
-        </TransparentButton>
+      <TransparentButton
+        className={cx('btnControl', 'btnScrollLeft', {
+          hideBtn: !(scrollState == SCROLL_MIDDLE || scrollState == SCROLL_END),
+        })}
+        onClick={() => onClickScroll(-1)}
+      >
+        <KeyboardArrowLeftRoundedIcon fontSize="large" />
+      </TransparentButton>
+      <TransparentButton
+        className={cx('btnControl', 'btnScrollRight', {
+          hideBtn: !(
+            scrollState == SCROLL_MIDDLE || scrollState == SCROLL_START
+          ),
+        })}
+        onClick={() => onClickScroll(1)}
+      >
+        <KeyboardArrowRightRoundedIcon fontSize="large" />
+      </TransparentButton>
     </div>
   );
 }
---
+++ src/modules/isedol/VideoContentList.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react';
+
+import { Waktaverse } from '../../common/constants';
+import * as func from '../../common/functions';
+import * as videoApi from '../../services/everywak.video';
+
+import HorizontalScrollableList from '../../common/Components/HorizontalScrollableList/HorizontalScrollableList';
+
+import VideoItem from './VideoItem';
+
+import './VideoContentList.scss';
+import cx from 'classnames';
+
+export interface Props {
+  className?: string;
+  options: {
+    type: 'all' | 'youtubeVideo' | 'youtubeClip' | 'youtubeVOD';
+    orderBy?: 'time' | 'time_oldest' | 'view';
+    twitchId?: string;
+    queryTxt?: string;
+    beginAt?: string;
+    endAt?: string;
+  };
+  type?: 'horizontal' | 'card';
+  size?: 'small' | 'normal';
+  shorts?: boolean;
+  hideProfileCircle?: boolean;
+  backgroundColor?: string;
+}
+
+function VideoContentList(props: Props) {
+  const {
+    className,
+    options,
+    type,
+    size = 'normal',
+    shorts = false,
+    hideProfileCircle = false,
+    backgroundColor = '#ffffff',
+    ...rest
+  } = props;
+
+  const [videoList, setVideoList] = useState([]);
+
+  const fetchVideoContent = async () => {
+    if (shorts) {
+      options.queryTxt = 'horts';
+    }
+
+    const response = await videoApi.getVideos(options as any);
+    const { videoList } = response.result;
+
+    const urlPrefix = shorts
+      ? 'https://www.youtube.com/shorts/'
+      : 'https://youtu.be/';
+
+    if (videoList) {
+      setVideoList(
+        videoList.map((item: any) => {
+          const target = Waktaverse.find(
+            (mb) => mb.login_name === item.twitchId,
+          )!;
+          const thumbnails = JSON.parse(item.thumbnails);
+          const thumbnail = thumbnails.high ||
+            thumbnails.medium ||
+            thumbnails.default || { url: '' };
+          return {
+            key: `video-${item.videoId}`,
+            href: `${urlPrefix}${item.videoId}`,
+            thumbnail: thumbnail.url,
+            title: item.title,
+            datetime: item.publishedAt * 1000,
+            formattedDateTime: func.formatDateTimeString(
+              new Date(item.publishedAt * 1000),
+            ),
+            author: item.nickname,
+            duration: item.duration,
+            viewCount: item.viewCount,
+            authorProfileImg: target.profileImg?.replace('{size}', '240'),
+          };
+        }),
+      );
+    }
+  };
+  useEffect(() => {
+    fetchVideoContent();
+  }, [options, shorts]);
+
+  const list = videoList.map((item) => (
+    <VideoItem {...(item as any)} size={size} />
+  ));
+  return (
+    <div
+      className={cx('VideoContentList', className, {
+        shorts,
+        hideProfileCircle,
+      })}
+      {...rest}
+    >
+      <HorizontalScrollableList backgroundColor={backgroundColor}>
+        <ul className="list">{list}</ul>
+      </HorizontalScrollableList>
+    </div>
+  );
+}
+
+export default VideoContentList;
