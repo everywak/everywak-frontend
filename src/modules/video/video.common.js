4dc96b9c7ed8ad6f038fb906c87976f5cb685e8c
wei756 <kjhoon1122@naver.com>
2025-04-08 06:27:49 +0900

    존재하지 않는 export 제거, 코드 포맷팅

--- src/modules/video/video.common.js
+++ src/modules/video/video.common.js
@@ -1,21 +1,27 @@
-
 /**
  * 현재 시각을 기준으로 date의 상대시간을 출력합니다.
- * @param {Date} date 
+ * @param {Date} date
  * @returns {string}
  */
-const getRelativeDateString = date => {
-  const today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()).getTime();
-  const target = new Date(date.getFullYear(), date.getMonth(), date.getDate()).getTime();
+const getRelativeDateString = (date) => {
+  const today = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    new Date().getDate(),
+  ).getTime();
+  const target = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+  ).getTime();
   const dateCount = (today - target) / MILLISECONDS_OF_DAY;
   if (dateCount === 0) {
     return '오늘';
   } else if (dateCount === 1) {
     return '어제';
-  } else  {
+  } else {
     return `${dateCount}일 전`;
   }
-}
+};
 
-
-export { orderNickname, MILLISECONDS_OF_DAY, personalColor, getRelativeDateString };
+export { MILLISECONDS_OF_DAY, personalColor, getRelativeDateString };
