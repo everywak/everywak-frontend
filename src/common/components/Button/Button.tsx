32193cddfd9b078aee632e1ca194880058164548
wei756 <kjhoon1122@naver.com>
2025-04-10 05:51:51 +0900

    버튼 컴포 링크 기능 추가

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 
 import styles from './Button.module.scss';
@@ -19,10 +20,12 @@ export type ButtonProps = {
     | 'white-transparent'
     | 'red-transparent';
   size?: 'small' | 'medium' | 'large';
+  href?: string;
+  target?: string;
   hideLabel?: boolean;
   disabled?: boolean;
   outlined?: boolean;
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => any;
+  onClick?: (e: React.MouseEvent) => any;
   children?: React.ReactNode;
   [key: string]: any;
 };
@@ -31,27 +34,43 @@ export function Button({
   className = '',
   color = 'primary',
   size = 'medium',
+  href,
+  target,
   hideLabel = false,
   disabled = false,
   outlined = false,
-  onClick = (e: React.MouseEvent<HTMLButtonElement>) => {},
+  onClick = () => {},
   children,
   ...rest
 }: ButtonProps): React.ReactElement {
-  return (
-    <button
-      className={clsx(
-        'Button',
-        styles.container,
-        className,
-        { [styles.hideLabel]: hideLabel, [styles.disabled]: disabled, [styles.outlined]: outlined },
-        styles[size],
-        styles[color],
-      )}
-      onClick={onClick}
-      {...rest}
-    >
-      {children}
-    </button>
-  );
+  const props = {
+    className: clsx(
+      'Button',
+      styles.container,
+      className,
+      { [styles.hideLabel]: hideLabel, [styles.disabled]: disabled, [styles.outlined]: outlined },
+      styles[size],
+      styles[color],
+    ),
+    onClick: onClick,
+    ...rest,
+  };
+  if (href) {
+    if (href.startsWith('https:') || href.startsWith('http:')) {
+      // 외부 링크
+      return (
+        <a {...props} href={href} target={target ?? '_blank'} rel="noopener noreferrer">
+          {children}
+        </a>
+      );
+    } else {
+      // 내부 링크
+      return (
+        <Link {...props} to={href} target={target ?? '_self'}>
+          {children}
+        </Link>
+      );
+    }
+  }
+  return <button {...props}>{children}</button>;
 }
