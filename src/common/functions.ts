1864a017d18b9dfca3f5cf66b2ef88a0bb78cfbc
wei756 <kjhoon1122@naver.com>
2025-04-11 14:36:49 +0900

    미사용 공통 함수 제거, 함수 스타일 화살표로 통일

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -1,7 +1,7 @@
 /**
  * @description URL 파라미터를 object화하여 반환합니다.
  */
-export function getURLParams(url: string) {
+export const getURLParams = (url: string) => {
   const param: Record<string, string> = {};
 
   const params = new URLSearchParams(url);
@@ -11,29 +11,29 @@ export function getURLParams(url: string) {
   });
 
   return param;
-}
+};
 
 /**
  * @description date가 input[type=date]의 value로 맞는 형태인지 여부를 반환합니다.
  */
-export function isDateStr(date: string) {
+export const isDateStr = (date: string) => {
   const dateStrRegExp = /([0-9]{4}-[0-9]{2}-[0-9]{2})/;
   return date.length === 10 && dateStrRegExp.test(date);
-}
+};
 
 /**
  * @description 숫자를 1000단위로 컴마가 삽입된 문자열로 변환합니다.
  */
-export function formatNumberWithCommas(n: number) {
+export const formatNumberWithCommas = (n: number) => {
   const parts = n.toString().split('.');
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   return parts.join('.');
-}
+};
 
 /**
  * @description 시각을 간략한 문자열로 반환합니다.
  */
-export function formatDateTimeString(date: Date) {
+export const formatDateTimeString = (date: Date) => {
   const today = new Date();
   const diffTime = (today.getTime() - date.getTime()) / 1000;
 
@@ -50,12 +50,12 @@ export function formatDateTimeString(date: Date) {
   } else {
     return Math.floor(diffTime / 60 / 60 / 24 / 30 / 12) + '년 전';
   }
-}
+};
 
 /**
  * @description 정수를 간략한 문자열로 반환합니다.
  */
-export function formatNumberShort(n: number) {
+export const formatNumberShort = (n: number) => {
   if (n < 1000) {
     return '' + n;
   } else if (n < 10000) {
@@ -65,49 +65,41 @@ export function formatNumberShort(n: number) {
   } else {
     return `${Math.floor(n / 10000000) / 10}억`;
   }
-}
+};
 
 /**
  * @description 브라우저 제목을 변경합니다.
  */
-export function setBrowserTitle(title: string) {
+export const setBrowserTitle = (title: string) => {
   document.title = `에브리왁굳 : ${title}`;
-}
+};
 
 /**
  * @description 랜덤 문자열을 생성합니다.
  */
-export function getRandomString(length = 16) {
+export const getRandomString = (length = 16) => {
   const char = '0123456789abcdefghijklmnopqrstuvwxyz';
   return Array(length)
     .fill(0)
     .map((v) => char[Math.floor(Math.random() * (char.length - 1))])
     .join('');
-}
+};
 
 /**
  * @description targetElement로 전체화면을 띄웁니다.
  */
-export function enableFullscreen(targetElement: HTMLElement) {
+export const enableFullscreen = (targetElement: HTMLElement) => {
   if (!document.fullscreenElement) {
     targetElement?.requestFullscreen();
   }
-}
+};
 /**
  * @description 전체화면을 해제합니다.
  */
-export function disableFullscreen() {
+export const disableFullscreen = () => {
   if (document.fullscreenElement) {
     document.exitFullscreen();
   }
-}
-
-export function setLocalStorage(key: string, value: any) {
-  return window.localStorage.setItem(key, JSON.stringify(value));
-}
-
-export function getLocalStorage(key: string) {
-  return JSON.parse(window.localStorage.getItem(key) as string);
-}
+};
 
 export const isExternalUrl = (url: string) => url.startsWith('https:') || url.startsWith('http:');
